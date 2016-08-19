838ba4dbe2058b33995296d644a95dbd6492b321
Ved Patel <vedmpatel@gmail.com>
2016-08-20 04:35:01 +0530

    added migrations

--- database/migrations/2016_08_19_223256_create_students_table.php
+++ database/migrations/2016_08_19_223256_create_students_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('reg_no');
+            $table->date('dob');
+
+            $table->unique('reg_no');
+
+            $table->rememberToken();
         });
     }
 
@@ -29,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('students');
     }
 }
