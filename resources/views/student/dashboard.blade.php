d29fd59d0486956d1de8e99eddb90daf3a186d49
Chetan Jain <Chetan Jain>
2016-08-26 05:08:34 +0530

    added dashboard

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -1,74 +1,92 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Dashboard</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        
         <link rel="stylesheet" type="text/css" href="{{ URL::to('assets/css/bootstrap.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{ URL::to('assets/css/mdb.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{ URL::to('assets/css/mdb.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ URL::to('assets/material-bootstrap/css/mdb.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{ URL::to('assets/student/css/dashboard.css')}}">
 
         
     </head>
     <body>
-        
-<!-- SideNav slide-out button -->
-<a href="#" data-activates="slide-out" class="button-collapse"><i class="fa fa-bars"></i></a>
-<!--/. SideNav slide-out button -->
+        <div class="wrapper">
+        <div class="sidebar" data-image="assets/img/sidebar-5.jpg">
 
-<!-- Sidebar navigation -->
-<ul id="slide-out" class="side-nav fixed default-side-nav light-side-nav">
-
-    <!-- Logo -->
-    <div class="logo-wrapper waves-light">
-        <a href="#"><img src="http://mdbootstrap.com/wp-content/uploads/2015/12/mdb-white2.png" class="img-fluid flex-center"></a>
-    </div>
-    <!--/. Logo -->
-
-    <!--Search Form-->
-    <form class="search-form" role="search">
-        <div class="form-group waves-effect">
-            <input type="text" class="form-control" placeholder="Search">
+            <div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="#" class="simple-text">
+                    MASTER ASSIST
+                </a>
+            </div>
+            <div class="profile-userpic">
+                    <img src="assets/student/img/upic1.png" class="img-responsive" alt="">
+                </div>
+                
+            
+            <ul class="nav">
+                <li>
+                    <a href="#">
+                        
+                        <p>DASHBOARD</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        
+                        <p>CHANNELS</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        
+                        <p>ACCOUNT</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        
+                        <p>HELP</p>
+                    </a>
+                </li>
+            </div>
         </div>
-    </form>
-    <!--/.Search Form-->
+        <div class="main-panel">
+        <nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                           <a href="">
+                               Home
+                            </a>
+                        </li>
+                        
+                        <li>
+                            <a href="#">
+                                Log out
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-    <!-- Side navigation links -->
-    <ul class="collapsible collapsible-accordion">
-        <li><a class="collapsible-header waves-effect">Click me</a>
-            <div class="collapsible-body">
-                <ul>
-                    <li><a href="#" class="waves-effect">Link</a>
-                    </li>
-                    <li><a href="#" class="waves-effect">Link</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li><a class="collapsible-header waves-effect">Click me</a>
-            <div class="collapsible-body">
-                <ul>
-                    <li><a href="#" class="waves-effect">Link</a>
-                    </li>
-                    <li><a href="#" class="waves-effect">Link</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-        <li><a class="collapsible-header waves-effect">Click me</a>
-            <div class="collapsible-body">
-                <ul>
-                    <li><a href="#" class="waves-effect">Link</a>
-                    </li>
-                    <li><a href="#" class="waves-effect">Link</a>
-                    </li>
-                </ul>
-            </div>
-        </li>
-    </ul>
-    <!--/. Side navigation links -->
-</ul>
-<!--/. Sidebar navigation -->
 
-    </body>
+
+
+</div>
+</body>
+
+        
 </html>
