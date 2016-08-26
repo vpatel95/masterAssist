32bb533b1554e1e53c3a2cdf23bd75292f650fcf
Ved Patel <vedmpatel@gmail.com>
2016-08-26 07:49:30 +0530

    added theme and font-awesome

---
+++ resources/views/login/includes/scripts.blade.php
@@ -0,0 +1,7 @@
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<script type="text/javascript" src="{{ URL::to('assets/js/jquery-1.10.2.js') }}"></script>
+<script type="text/javascript" src="{{ URL::to('assets/js/bootstrap.min.js') }}"></script>
