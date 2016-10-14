89de92e6e9391474730859fbfc0a96bed9400f86
Ved Patel <vedmpatel@gmail.com>
2016-10-14 23:11:03 +0530

    Added Individual Student Teacher Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,17 +7,20 @@ use Illuminate\Http\Request;
 use App\User;
 use App\Student;
 use App\Teacher;
+use App\Http\Response;
 use App\Http\Requests;
 
 class ProfileController extends Controller
 {
     public function teacherShow($id) {
-    	$teacher = Teacher::where('teacher_id', $id);
+    	$teacher = Teacher::where('teacher_id', $id)->first();
 
-    	return($teacher->first_name);
+    	return $teacher->first_name;
     }
 
     public function studentShow($id) {
+    	$student = Student::where('student_id', $id)->first();
 
+    	return $student->first_name;
     }
 }
--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -92,18 +92,22 @@
       <main class="mdl-layout__content mdl-color--grey-100">
         <div class="mdl-grid demo-content">
           <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
-            <a href="{{ route('teacher.profile', '123456') }}">
+            
               <svg fill="currentColor" width="200px" height="200px" viewBox="0 0 1 1" class="demo-chart mdl-cell mdl-cell--4-col mdl-cell--3-col-desktop">
-                <use xlink:href="#piechart" mask="url(#piemask)" />
-                <text x="0.5" y="0.5" font-family="Roboto" font-size="0.3" fill="#888" text-anchor="middle" dy="0.1">82<tspan font-size="0.2" dy="-0.07">%</tspan></text>
+                <a href="{{ route('teacher.profile', '123456') }}">
+                  <use xlink:href="#piechart" mask="url(#piemask)" />
+                  <text x="0.5" y="0.5" font-family="Roboto" font-size="0.3" fill="#888" text-anchor="middle" dy="0.1">82<tspan font-size="0.2" dy="-0.07">%</tspan></text>
+                </a>
               </svg>
-            </a>
-            <a href="{{ route('teacher.profile', '2') }}">
+            
+            
               <svg fill="currentColor" width="200px" height="200px" viewBox="0 0 1 1" class="demo-chart mdl-cell mdl-cell--4-col mdl-cell--3-col-desktop">
-                <use xlink:href="#piechart" mask="url(#piemask)" />
-                <text x="0.5" y="0.5" font-family="Roboto" font-size="0.3" fill="#888" text-anchor="middle" dy="0.1">82<tspan dy="-0.07" font-size="0.2">%</tspan></text>
+                <a href="{{ route('teacher.profile', '2') }}">
+                  <use xlink:href="#piechart" mask="url(#piemask)" />
+                  <text x="0.5" y="0.5" font-family="Roboto" font-size="0.3" fill="#888" text-anchor="middle" dy="0.1">82<tspan dy="-0.07" font-size="0.2">%</tspan></text>
+                </a>
               </svg>
-            </a>
+            
             <svg fill="currentColor" width="200px" height="200px" viewBox="0 0 1 1" class="demo-chart mdl-cell mdl-cell--4-col mdl-cell--3-col-desktop">
               <use xlink:href="#piechart" mask="url(#piemask)" />
               <text x="0.5" y="0.5" font-family="Roboto" font-size="0.3" fill="#888" text-anchor="middle" dy="0.1">82<tspan dy="-0.07" font-size="0.2">%</tspan></text>
