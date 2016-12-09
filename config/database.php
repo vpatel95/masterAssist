0659b85c87a319af8731d6982851c8cfa6f4c2d1
Ved Patel <vedmpatel@gmail.com>
2016-12-09 17:32:16 +0530

    Added travis

--- config/database.php
+++ config/database.php
@@ -78,6 +78,18 @@ return [
             'schema' => 'public',
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'forge_test'),
+            'username'  => env('DB_TEST_USERNAME', 'forge'),
+            'password'  => env('DB_TEST_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
     ],
 
     /*
