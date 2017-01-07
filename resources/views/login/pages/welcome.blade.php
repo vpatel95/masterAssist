b0f6bbf4ea2adde41450b90a1dfc5f25e897550c
Ved Patel <vedmpatel@gmail.com>
2017-01-07 16:37:01 +0530

    cleaned welcome page

--- resources/views/login/pages/welcome.blade.php
+++ resources/views/login/pages/welcome.blade.php
@@ -19,20 +19,23 @@
         <h1 >Features</h1>
         <hr class="introhr">
         <div class="row " style="margin-top:5.5%;">
-           
             <div class="col-sm-4 f1" >
-             <center></center><div class="fahover"><i class="fa fa-location-arrow" aria-hidden="true"></i></div></center>
+                <center>
+                    <div class="fahover">
+                        <i class="fa fa-location-arrow" aria-hidden="true"></i>
+                    </div>
+                </center>
                 <!--<img src="{{ URL::to('assets/login/img/teacher.png')}}" class="img-responsive teacherimg" " alt="Responsive image">-->
                 <h2 style="">Lorem Ipsum</h2>
                 <center><p>loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren.</p></center>
             </div>
             <div class="col-sm-4 f2">
-             <div class="fahover"><i class="fa fa-pencil-square" aria-hidden="true"></i></div>
+                <div class="fahover"><i class="fa fa-pencil-square" aria-hidden="true"></i></div>
                 <h2 style="">Lorem Ipsum</h2>
                 <center><p>loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren.</p></center>
             </div>    
             <div class="col-sm-4 f3">
-            <div class="fahover"> <i class="fa fa-plus-circle" aria-hidden="true"></i></div>
+                <div class="fahover"> <i class="fa fa-plus-circle" aria-hidden="true"></i></div>
                 <h2 style="">Lorem Ipsum</h2>
                 <center><p>loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren.</p></center>
             </div>
@@ -98,31 +101,26 @@
 
     <!-- Modal content-->
         <div class="modal-content" >
-        <div class="modal-header" >
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">LOGIN</h4>
+            <div class="modal-header" >
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">LOGIN</h4>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('login') }}" method="POST">
+                    <div class="form-group">
+                        <input type="email" name="email" placeholder="Email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" placeholder="Password" class="form-control">
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary" style="margin-left: 87%;border-radius:0px;">Log In</button>
+                        <input type="hidden" name="_token" value="{{ Session::token() }}">
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="modal-body">
-            <form action="{{ route('login') }}" method="POST">
-                <div class="form-group">
-                    <input type="email" name="email" placeholder="Email" class="form-control">
-                </div>
-                <div class="form-group">
-                    <input type="password" name="password" placeholder="Password" class="form-control">
-                </div>
-                <div>
-                    <button type="submit" class="btn btn-primary" style="margin-left: 87%;border-radius:0px;">Log In</button>
-                    <input type="hidden" name="_token" value="{{ Session::token() }}">
-                </div>
-            </form>
-        </div>
-      
-        </div>
-
     </div>
 </div>
-
-<script>
-</script>
 
 @endsection
