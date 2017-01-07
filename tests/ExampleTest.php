24afd918babbd671b2283d21b2d1a0b108e7739e
Ved Patel <vedmpatel@gmail.com>
2017-01-07 17:29:47 +0530

    test case update

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,6 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visitRoute('home');
+        $this->visit('/')
+             ->visitRoute('home');
     }
 }
